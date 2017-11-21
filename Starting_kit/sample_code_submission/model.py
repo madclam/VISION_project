d0c72fd2e9a54e639e829afaa3db82ba1a0a3a93
Madclam <madclam@gmail.com>
2017-11-20 18:25:14 -0800

    starting kit corrections

--- Starting_kit/sample_code_submission/model.py
+++ Starting_kit/sample_code_submission/model.py
@@ -52,7 +52,7 @@ class model:
         else:
             print("Model already load")
         
-    def fit(self, X, y):
+    def fit(self, X, Y):
         '''
         This function should train the model parameters.
         Here we do nothing in this example...
@@ -66,18 +66,21 @@ class model:
         Use data_converter.convert_to_num() to convert to the category number format.
         For regression, labels are continuous values.
         '''
-        self.num_train_samples = len(X)
-        print(self.num_train_samples)
-        if X.ndim>1: self.num_feat = len(X[0])
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = len(y)
-        if y.ndim>1: self.num_labels = len(y[0])
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
+        # For multi-class problems, convert target to be scikit-learn compatible
+        # into one column of a categorical variable
+        y=self.convert_to_num(Y)     
+        
+        self.num_train_samples = X.shape[0]
+        if X.ndim>1: self.num_feat = X.shape[1] # Does not work for sparse matrices
+        print("FIT: dim(X)= [{:d}, {:d}]").format(self.num_train_samples, self.num_feat)
+        num_train_samples = y.shape[0]
+        if y.ndim>1: self.num_labels = y.shape[1]
+        print("FIT: dim(y)= [{:d}, {:d}]").format(num_train_samples, self.num_labels)
         if (self.num_train_samples != num_train_samples):
             print("ARRGH: number of samples in X and y do not match!")
-        self.is_trained=True
-        
+
         self.mod.fit(X, y)
+        self.is_trained=True
 
     def predict(self, X):
         '''
@@ -91,13 +94,15 @@ class model:
         Scikit-learn also has a function predict-proba, we do not require it.
         The function predict eventually can return probabilities.
         '''
-        num_test_samples = len(X)
-        if X.ndim>1: num_feat = len(X[0])
+        num_test_samples = X.shape[0]
+        if X.ndim>1: num_feat = X.shape[1]
         print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
         if (self.num_feat != num_feat):
             print("ARRGH: number of features in X does not match training data!")
         print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = self.mod.predict(X)
+        
+        # Return predictions as class probabilities
+        y = self.mod.predict_proba(X)
         return y
 
     def save(self, path="./"):
@@ -112,3 +117,14 @@ class model:
                 self = pickle.load(f)
             print("Model reloaded from: " + modelfile)
         return self
+        
+    def convert_to_num(self, Ybin, verbose=True):
+        ''' Convert binary targets to numeric vector (typically classification target values)'''
+        if verbose: print("Converting to numeric vector")
+        Ybin = np.array(Ybin)
+        if len(Ybin.shape) ==1: return Ybin
+        classid=range(Ybin.shape[1])
+        Ycont = np.dot(Ybin, classid)
+        if verbose: print Ycont
+        return Ycont
+ 
